9335941db996a7e874f9e0e03188a2a80b4bd004
Muhammed Utku AKSU <117573765+utkuu0@users.noreply.github.com>
2024-07-10 14:38:46 +0300

    Add files via upload

---
+++ Eslestirme/Kart.java
@@ -0,0 +1,27 @@
+package Eslestirme;
+
+public class Kart {
+    private char deger;
+    private boolean tahmin = false;
+
+    public Kart(char deger) {
+        this.deger = deger;
+    }
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+    
+}
